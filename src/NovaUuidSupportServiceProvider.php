9117d7bc531e51eed6a23517bfcd4a7c4b84e447
woutersioen <wouter.sioen@gmail.com>
2019-11-21 16:56:27 +0100

    Add a dummy service provider
    We'll load migrations in there later. Note that I changed the psr4
    autoloader to make sure clashes with other madewithlove projects are
    less likely.

---
+++ src/NovaUuidSupportServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Madewithlove\NovaUuidSupport;
+
+use Illuminate\Support\ServiceProvider;
+
+class NovaUuidSupportServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // @todo: load migrations
+    }
+}
